6bdb276916796c81ddda27ca4d156afc4184a7b1
NascimentoTalesDev <nascimentotalesdev@gmail.com>
2024-11-23 01:17:01 -0300

    feat: add menu mobile

--- src/app/_components/nav/index.tsx
+++ src/app/_components/nav/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react";
+import React from "react";
 import { NavBettewen, NavComponent, NavContainer } from "./components";
 import Logo from "../Logo";
 import NavGroupLinkDesk from "./group-link";
