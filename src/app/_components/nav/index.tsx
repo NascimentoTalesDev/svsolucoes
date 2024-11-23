4df4fe99d4f84463fcc5aaea1a2e1bb0407fb681
NascimentoTalesDev <nascimentotalesdev@gmail.com>
2024-11-23 12:29:12 -0300

    online

--- src/app/_components/nav/index.tsx
+++ src/app/_components/nav/index.tsx
@@ -14,6 +14,7 @@ const NavBar = () => {
             <NavContainer>
                 <NavBettewen>
                     <Logo />
+                    <Logo />
                     <NavGroupLinkDesk className={"hidden md:flex"} />
                     <div onClick={()=> menuMobile.onOpen()} className="md:hidden p-4 cursor-pointer">
                         <AlignJustify width={30} className="text-white" />
